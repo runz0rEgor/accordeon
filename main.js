0615e364af766eae1999addddd4612344ba9d075
runz0rEgor <egor31340@gmail.com>
2021-01-21 11:53:42 +0300

    finaly fix

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
-const btn = document.querySelectorAll(".accordeon");
+// const btn = document.querySelectorAll(".accordeon");
+
+// const showList = () => {
+//   btn.forEach((item) => {
+//     item.addEventListener("click", (e) => {
+//       e.preventDefault();
+//       e.target.nextElementSibling.classList.toggle('active')
+//       // item.nextElementSibling.classList.toggle("active");      
+//     });
+//   });
+// };
+
+// showList();
+
+const wrapper = document.querySelector('.wrapper')
 
 const showList = () => {
-  btn.forEach((item) => {
-    item.addEventListener("click", (e) => {
-      e.preventDefault();
-      item.nextElementSibling.classList.toggle("active");
-    });
-  });
-};
+  wrapper.addEventListener('click', e => {
+    if(e.target.classList.contains('accordeon')) {
+      e.target.nextElementSibling.classList.toggle('active')
+    }
+  })
+}
 
-showList();
+showList()
